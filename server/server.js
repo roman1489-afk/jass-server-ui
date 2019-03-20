fc6156a751211359518b3dbc0f2b1772d00a2f3d
Joel Niklaus <me@joelniklaus.ch>
2019-03-21 00:00:29 +0800

    made server only start on https when port is set to 443

--- server/server.js
+++ server/server.js
@@ -7,42 +7,44 @@ import {Logger} from './logger';
 let server;
 
 export function start(port, app) {
+	if (port === 443)
+		try {
+			// Run These commands on the production server to generate the certificates
+			// sudo certbot certonly --manual
+			// or sudo certbot certonly --standalone -d jass.joeli.to
+			// openssl dhparam -out /var/www/jass.joeli.to/sslcert/dh-strong.pem 2048
 
-	try {
-		// Run These commands on the production server to generate the certificates
-		// sudo certbot certonly --manual
-		// or sudo certbot certonly --standalone -d jass.joeli.to
-		// openssl dhparam -out /var/www/jass.joeli.to/sslcert/dh-strong.pem 2048
+			Logger.info('Starting https server...');
+			let fs = require('fs');
+			const options = {
+				key: fs.readFileSync('/etc/letsencrypt/live/jass.joeli.to/privkey.pem'),
+				cert: fs.readFileSync('/etc/letsencrypt/live/jass.joeli.to/fullchain.pem'),
+				//dhparam: fs.readFileSync('/var/www/jass.joeli.to/sslcert/dh-strong.pem')
+			};
 
-		Logger.info('Starting https server...');
-		let fs = require('fs');
-		const options = {
-			key: fs.readFileSync('/etc/letsencrypt/live/jass.joeli.to/privkey.pem'),
-			cert: fs.readFileSync('/etc/letsencrypt/live/jass.joeli.to/fullchain.pem'),
-			//dhparam: fs.readFileSync('/var/www/jass.joeli.to/sslcert/dh-strong.pem')
-		};
+			let helmet = require('helmet');
+			app.use(helmet());
 
-		let helmet = require('helmet');
-		app.use(helmet());
+			server = https.createServer(options, app);
+		} catch (e) {
+			Logger.info(e);
+			Logger.info('There has been a problem starting the https server. Starting a normal http server now...');
 
-		server = https.createServer(options, app);
-	} catch (e) {
-		Logger.info(e);
-		Logger.info('There has been a problem starting the https server. Starting a normal http server now...');
-
+			server = http.createServer(app);
+		}
+	else
 		server = http.createServer(app);
-	}
 
-    new WebSocketServer({server}).on('connection', (ws) => {
-        SessionHandler.handleClientConnection(ws);
-    });
+	new WebSocketServer({server}).on('connection', (ws) => {
+		SessionHandler.handleClientConnection(ws);
+	});
 
-    server.listen(port, () => {
-        Logger.info(`Server listening on port: ${server.address().port}`);
-    });
+	server.listen(port, () => {
+		Logger.info(`Server listening on port: ${server.address().port}`);
+	});
 }
 
 export function stop() {
-    SessionHandler.resetInstance();
-    server.close();
+	SessionHandler.resetInstance();
+	server.close();
 }
